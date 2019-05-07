b463121b38b2fdc975fcfcec1d73f5115422dd09
rc0032990 <rc0032990@CRO-24-05>
2019-05-07 10:43:24 +0100

    triangle,square, loop 1

--- delay move picture/Form1.cs
+++ delay move picture/Form1.cs
@@ -36,6 +36,23 @@ namespace delay_move_picture
             pictureBox1.Location = new Point(pictureBox1.Location.X - 50 , pictureBox1.Location.Y + 50 );
             await Task.Delay(500);
             pictureBox1.Location = new Point(pictureBox1.Location.X , pictureBox1.Location.Y - 100);
+            await Task.Delay(500);
         }
-    }
-}
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            while (pictureBox1.Location.X + 5 < ClientRectangle.Width - pictureBox1.Width)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+                await Task.Delay(100);
+            }
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+                await Task.Delay(100);
+            }
+        }
